35c59efdc35d74909b1808d2fbe46f6bb9531783
fkeyz <fkeyzuwu@gmail.com>
2024-01-06 02:05:47 +0200

    added my own stuff :3

---
+++ src/entity.h
@@ -0,0 +1,24 @@
+#include <raylib-cpp.hpp>
+
+class Entity
+{
+public:
+    raylib::Vector2 position;
+    raylib::Vector2 size;
+    float speed;
+    raylib::Color color;
+
+    Entity(raylib::Vector2 _position, raylib::Vector2 _size, float _speed, raylib::Color _color):
+        position(_position), size(_size), speed(_speed), color(_color){};
+
+    Rectangle get_rect() const
+    {
+        return Rectangle
+        {
+            position.x - size.x / 2,
+            position.y - size.y / 2, // might need to change it to + instead of -
+            size.x,
+            size.y
+        };
+    }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,21 @@
 #include <raylib-cpp.hpp>
+#include "entity.h"
+#include "vector"
+
+Entity* spawn_enemy()
+{
+    return new Entity(
+        Vector2{float(GetRandomValue(0, GetScreenWidth())), float(GetRandomValue(0, GetScreenHeight()))},
+        Vector2{15, 15},
+        50,
+        GREEN
+    );
+}
+
+void draw_entity(const Entity& entity)
+{
+    DrawRectangleRec(entity.get_rect(), entity.color);
+}
 
 int main() {
     
@@ -11,17 +28,65 @@ int main() {
     
     SetTargetFPS(60);
 
+    Entity player (
+        Vector2{float(screenWidth) / 2, float(screenHeight) / 2},
+        Vector2{30, 30},
+        150,
+        RED
+    );
+
+    std::vector<Entity*> enemies;
+
+    float spawn_timer = 0;
+    float spawn_interval = 2;
+
     // Main game loop
     while (!w.ShouldClose()) // Detect window close button or ESC key
     {
-        // Update
+        float delta = GetFrameTime();
+        spawn_timer += delta;
+        if(spawn_timer > spawn_interval)
+        {
+            spawn_timer = 0;
+            enemies.push_back(spawn_enemy());
+        }
 
-        // TODO: Update your variables here
+        raylib::Vector2 input = {0};
 
-        // Draw
+        if (IsKeyDown(KEY_A)) input.x -= 1;
+        if (IsKeyDown(KEY_D)) input.x += 1;
+        if (IsKeyDown(KEY_W)) input.y -= 1;
+        if (IsKeyDown(KEY_S)) input.y += 1;
+        
+        raylib::Vector2 direction = input.Normalize();
+        player.position += direction * player.speed * delta;
+        Rectangle player_rect = player.get_rect();
+
         BeginDrawing();
+
+        for(int i = enemies.size() - 1; i >= 0; i--) 
+        {
+            Entity* enemy = enemies.at(i);
+            Rectangle enemy_rect = enemy->get_rect();
+            if(CheckCollisionRecs(enemy_rect, player_rect))
+            {
+                enemies.erase(enemies.begin() + i);
+                player.speed += 10;
+                delete enemy;
+            }
+            else
+            {
+                raylib::Vector2 direction_to_player = (player.position - enemy->position).Normalize();
+                enemy->position += direction_to_player * enemy->speed * delta;
+                draw_entity(*enemy);
+            }
+        }
+
+        draw_entity(player);
         ClearBackground(RAYWHITE);
+        
         textColor.DrawText("Congrats! You created your first window!", 190, 200, 20);
+
         EndDrawing();
     }
 
